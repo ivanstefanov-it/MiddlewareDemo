8b12d905c4755b0536eaa2c5abf4b8e6b604451f
ivan.stefanov <ivanstefanov.it@gmail.com>
2022-11-15 10:29:45 +0200

    Initial Commit

---
+++ MiddlewareDemo.Web/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MiddlewareDemo.Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet(Name = "GetError")]
+        public IActionResult GetError()
+        {
+            throw new Exception();
+            //return Ok();
+        }
+    }
+}
---
+++ MiddlewareDemo.Web/Middlewares/GlobalExeptionHandlingMiddleware.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Text.Json;
+
+namespace MiddlewareDemo.Web.Middlewares
+{
+    public class GlobalExeptionHandlingMiddleware : IMiddleware
+    {
+        private readonly ILogger<GlobalExeptionHandlingMiddleware> _logger;
+
+        public GlobalExeptionHandlingMiddleware(ILogger<GlobalExeptionHandlingMiddleware> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, e.Message);
+
+                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+
+                ProblemDetails problem = new()
+                {
+                    Status = (int)HttpStatusCode.InternalServerError,
+                    Type = "Server Error",
+                    Title = "Server Error",
+                    Detail= "An internal server error has occurred"
+                };
+
+                string json = JsonSerializer.Serialize(problem);
+                await context.Response.WriteAsync(json);
+                context.Response.ContentType= "application/json";
+            }
+        }
+    }
+}
